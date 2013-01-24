ca80accdcc3ed4156401b4c20cb955bef7644b52
Peter Jones <pj@ukoki.com>
2013-01-24 13:36:46 +0800

    had to remove srandmember with count arg

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,17 @@
 class MainController < ApplicationController
   def index
-    @articles = $redis.srandmember('wikidata', 20)
+    #if Rails.env == 'production'
+    #  @articles = (1..20).map { |i| $redis.srandmember('wikidata') }
+    #else
+    #  @articles = $redis.srandmember('wikidata', 20)
+    #end
+    @articles = []
+    $redis.pipelined do
+      20.times do
+        @articles.push $redis.srandmember('wikidata')
+      end
+    end
+    @articles = @articles.map { |a| a.value }
     respond_to do |format|
       format.html
       format.json { render :json => @articles }
